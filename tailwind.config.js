e9a13180a3f0c7bb9710d0ad7fa344e1277d9a65
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-10-14 08:58:39 -0400

    Initialize shadcn/ui
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html.js.jsx,ts,tsx}'],
+    darkMode: ['class'],
+    content: ['./src/**/*.{html.js.jsx,ts,tsx}'],
   theme: {
-    extend: {
-      primary: '#0C0C0C',
-      secondary: '#1d1c1c',
-    },
+  	extend: {
+  		primary: '#0C0C0C',
+  		secondary: '#1d1c1c',
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
