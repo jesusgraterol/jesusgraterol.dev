6091aa181be7aaf5aeedc59bf46b3924ec3a2787
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-10-14 08:36:13 -0400

    Initialize styles, font and tailwind
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html.js.jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      primary: '#0C0C0C',
+      secondary: '#1d1c1c',
+    },
+  },
+  plugins: [],
+}
