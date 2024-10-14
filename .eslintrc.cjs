46bf48273fe4ee5a227c998071297b6987e1752a
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-10-14 08:21:31 -0400

    Setup ESLint
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,14 +5,33 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    'airbnb-base',
+    'airbnb-typescript/base'
   ],
+  parserOptions: {
+    project: './tsconfig.app.json'
+  },
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
+  overrides: [
+    {
+      files: [ '*.test-*.{ts,tsx}', "*.bench.ts" ],
+      rules: {
+        'object-curly-newline': 'off',
+        'import/no-extraneous-dependencies': [ 'error', { 'devDependencies': true } ]
+      }
+    }
+  ],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'no-multiple-empty-lines': [ 'error', { 'max': 5, 'maxEOF': 0 } ],
+    'no-underscore-dangle': 'off',
+    '@typescript-eslint/naming-convention': 'off',
+    'import/prefer-default-export': 'off',
+    'import/extensions': 'off'
   },
 }
