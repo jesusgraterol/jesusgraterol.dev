6094c3a5d3b1a5be2cd4502c8f464c00595e12e3
Jesus Graterol <jesusgraterol.dev@protonmail.com>
2024-10-16 18:08:33 -0400

    The bio section no longer uses a dialog to display all the text
    Signed-off-by: Jesus Graterol <jesusgraterol.dev@protonmail.com>

--- src/pages/bio/index.component.tsx
+++ src/pages/bio/index.component.tsx
@@ -1,12 +1,5 @@
-import { Button } from '@/shared/shadcn/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from '@/shared/shadcn/components/ui/dialog.tsx';
+import { useState } from 'react';
+import { Button } from '@/shared/shadcn/components/ui/button.tsx';
 
 /* ************************************************************************************************
  *                                         IMPLEMENTATION                                         *
@@ -16,55 +9,64 @@ import {
  * Bio
  * Component in charge of quickly showing what Jesus Graterol does.
  */
-const Bio = () => (
-  <section>
-    <header>
-      <h1
-        className='text-2xl font-semibold leading-none tracking-tight'
-      >Hi! My name is Jesus Graterol</h1>
-    </header>
+const Bio = () => {
+  /* **********************************************************************************************
+   *                                             STATE                                            *
+   ********************************************************************************************** */
+  const [readMore, setReadMore] = useState<boolean>(false);
 
-    <p className='my-3'>
-      I am a skilled Full-Stack Web Developer with 10+ years of experience in building scalable
-        web applications and REST-based APIs.
-    </p>
-    <p className='inline'>
-      My proficiency extends to data science, allowing...
-    </p>
 
-    <Dialog>
-      <DialogTrigger asChild>
-        <Button
-          variant='link'
-          className='text-xs sm:text-sm text-muted inline p-0 ml-2'
-        >
-          see more
-        </Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle className='text-left'>
-            Hi! My name is Jesus Graterol
-          </DialogTitle>
-          <DialogDescription></DialogDescription>
-        </DialogHeader>
 
-        <p>
-          I am a skilled Full-Stack Web Developer with 10+ years of experience in building scalable
-            web applications and REST-based APIs.
-        </p>
-        <p className='mt-1'>
-          My proficiency extends to data science, allowing me to create and serve complex machine
-            learning models over the Internet.
-        </p>
-        <p className='mt-1'>
-          I am a problem-solver with keen attention to detail, adept at identifying and resolving
-          complex issues promptly and professionally.
-        </p>
-      </DialogContent>
-    </Dialog>
-  </section>
-);
+
+
+  /* **********************************************************************************************
+   *                                           COMPONENT                                          *
+   ********************************************************************************************** */
+  return (
+    <section>
+      <header>
+        <h1
+          className='text-2xl font-semibold leading-none tracking-tight'
+        >Hi! My name is Jesus Graterol</h1>
+      </header>
+
+      <p className='my-3'>
+        I am a skilled Full-Stack Web Developer with 10+ years of experience in building scalable
+          web applications and REST-based APIs.
+      </p>
+
+      {
+        readMore
+          ? (
+            <>
+              <p>
+                My proficiency extends to data science, allowing me to create and serve complex
+                 machine learning models over the Internet.
+              </p>
+              <p className='mt-3'>
+                I am a problem-solver with keen attention to detail, adept at identifying and
+                 resolving complex issues promptly and professionally.
+              </p>
+            </>
+          )
+          : (
+            <>
+              <p className='inline'>
+                My proficiency extends to data science, allowing...
+              </p>
+              <Button
+                variant='link'
+                className='text-xs sm:text-sm text-muted inline p-0 ml-2'
+                onClick={() => setReadMore(true)}
+              >
+                see more
+              </Button>
+            </>
+          )
+      }
+    </section>
+  );
+};
 
 
 
